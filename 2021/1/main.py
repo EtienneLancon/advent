1b610d5ed19019e48941ce8caaa205ad8533ec33
etienne <lancon.etienne@gmail.com>
2022-12-08 23:04:06 +0100

    wouhou

--- 2021/1/main.py
+++ 2021/1/main.py
@@ -1,1 +1,25 @@
-print('coucou')
+file = open('./data', 'r')
+
+data = file.readlines()
+
+previous = 0
+greaterThanPrevious = 0
+
+numbers = []
+
+for i in range(0, len(data)):
+    numbers.append(int(data[i]))
+
+for i in range(2, len(numbers)):
+    sum = numbers[i]+numbers[i-1]+numbers[i-2]
+
+    if sum > previous & previous != 0:
+        greaterThanPrevious += 1
+
+    previous = sum
+
+
+
+file.close()
+
+print(greaterThanPrevious)
