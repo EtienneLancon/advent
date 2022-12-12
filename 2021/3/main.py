016b424838b20c5c914f97df817938427918c61c
etienne <lancon.etienne@gmail.com>
2022-12-12 17:02:47 +0100

    ergh

--- 2021/3/main.py
+++ 2021/3/main.py
@@ -1,31 +1,62 @@
+def selectCandidate(candidates, criteria):
+    i = 0
+    j = 0
+    while candidates.__len__() > 1:
+        bitCount = {'0': 0, '1': 0}
+        for candidate in candidates:
+            if candidate[i] == '0':
+                bitCount['0'] += 1
+            else:
+                bitCount['1'] += 1
+
+        toRemove = ''
+            
+        if criteria == 'most':
+            if bitCount['1'] >= bitCount['0']:
+                toRemove = '0'
+            else:
+                toRemove = '1'
+        else:
+            if bitCount['1'] < bitCount['0']:
+                toRemove = '0'
+            else:
+                toRemove = '1'
+
+        j = 0
+        while j < candidates.__len__():
+            if candidates[j][i] == toRemove:
+                candidates.pop(j)
+                j -= 1
+            j += 1
+        
+        i += 1
+
+    return candidates[0]
+
+def getDecimal(binarystring):
+    decimal = 0
+    length = binarystring.__len__()
+    for i in range(0, length):
+        if(binarystring[i] == '1'):
+            decimal += pow(2, length-i-1)
+
+    return decimal
+
+
 file = open('./data', 'r')
 
-bitCount = []
-first = True
+oxygenCandidates = []
+carbonCandidates = []
 
 for binary in file:
-    char = 0
-    for bit in binary.rstrip():
-        if first:
-            tmp = {'0': 0, '1': 0}
-            tmp[bit] += 1
-            bitCount.append(tmp)
-        else:
-            bitCount[char][bit] += 1
-            char += 1
-    
-    first = False
+    oxygenCandidates.append(binary.rstrip())
+    carbonCandidates.append(binary.rstrip())
 
-gamma = 0
-epsilon = 0
-length = bitCount.__len__()
 
-for i in range(0, length):
-    if bitCount[i]['1'] > bitCount[i]['0']:
-        epsilon += pow(2, length-i-1)
-    else:
-        gamma += pow(2, length-i-1)
 
-print(gamma*epsilon)
+oxygen = selectCandidate(oxygenCandidates, 'most')
+carbon = selectCandidate(carbonCandidates, 'least')
+
+print(getDecimal(oxygen)*getDecimal(carbon))
 
 file.close()
